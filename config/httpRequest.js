b70e07d2bad24933d13f070b4ca4346b6716d52a
Arufiand <alfiandannyarmanta@gmail.com>
2020-12-20 22:53:30 +0700

    create httpRequest (not yet implemented)

---
+++ config/httpRequest.js
@@ -0,0 +1,22 @@
+// import axios from 'axios';
+// import { useState, useEffect } from 'react';
+// // import Label from '../configs/label_local_storage';
+// import { useStore } from '../../App';
+
+// const httpRequest = () => {
+//   const { rcUserId, rcAuthToken } = useStore();
+
+//   const httpRequestRocketChat = axios.create({
+//     headers: {
+//       'X-Auth-Token': rcAuthToken,
+//       'X-User-Id': rcUserId
+//     },
+//   });
+
+//   return httpRequestRocketChat;
+// }
+
+// export default httpRequest;
+
+// // Documentation : 
+// // http://gitserver.medixsoft.co.id/workr-asik/mobile-partner-new/blob/development/app/configs/CustomAxios.js
